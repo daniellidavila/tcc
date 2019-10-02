ecbc5877d03ed62f03feec9db4a7b09959f50bda
daniellidavila <daniellidavila51@gmail.com>
2019-10-01 22:37:25 -0300

    first commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+import { CadastroPage } from '../cadastro/cadastro';
+
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  campoUsername: string;
+  campoSenha: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  goToTabsPage(){
+    if (this.campoUsername=="adm" &&  this.campoSenha=="123") {
+      this.navCtrl.push(TabsPage)
+    }
+  }
+
+  goToCadastro(){
+    this.navCtrl.push(CadastroPage)
+  }
+}
